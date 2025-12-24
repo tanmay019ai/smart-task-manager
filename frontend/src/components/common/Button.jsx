9fe6cce369060b93d427db1eb471adcaa275fbb7
tanmay019ai <tanmaysr019@gmail.com>
2025-12-24 11:18:11 +0530

    deleted

--- frontend/src/components/common/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button({ children, onClick, className = '', type = 'button' }) {
-  return (
-    <button type={type} onClick={onClick} className={`px-4 py-2 rounded bg-blue-600 text-white ${className}`}>
-      {children}
-    </button>
-  );
-}
--- frontend/src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loader() {
-  return (
-    <div className="flex items-center justify-center p-4">
-      <div className="w-8 h-8 border-4 border-blue-300 border-t-blue-600 rounded-full animate-spin" />
-    </div>
-  );
-}
